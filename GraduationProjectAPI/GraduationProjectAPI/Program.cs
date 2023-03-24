f649bda5f3efa86b780f7d5b103373a43238f5dc
hadelDabbas <hadeldabbas@gmail.com>
2023-03-24 21:53:27 +0200

    swagger

--- GraduationProjectAPI/GraduationProjectAPI/Program.cs
+++ GraduationProjectAPI/GraduationProjectAPI/Program.cs
@@ -1,6 +1,18 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+//for the swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseAuthentication();
+app.MapControllers();
+//end swagger
 app.Run();
